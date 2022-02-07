4dc482e76bff64ec90be7270b03ab6b9b1ddb8c8
Gwendolyn Friedman <gwenrfriedman@gmail.com>
2022-02-07 13:31:44 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Gwen Friedman on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
